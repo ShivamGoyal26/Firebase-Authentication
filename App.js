731f99424fb16f2f8a2bfe47d6ff529f57c41ee4
ShivamGoyal26 <officialshivamgoyal@gmail.com>
2021-01-20 12:53:57 +0530

    Async Storage Done

--- App.js
+++ App.js
@@ -1,10 +1,35 @@
-import React from 'react';
+import React, {useEffect} from 'react';
 import AuthScreens from './Navigators/Navigator';
+import AsyncStorage from '@react-native-async-storage/async-storage';
+import LoginScreen from './screens/LoginScreen';
 
 const App = () => {
-  return(
-   <AuthScreens></AuthScreens>
-  );
+  const [isFirstLaunch, setIsFirstLaunch] = React.useState(null);
+
+  useEffect(() => {
+    AsyncStorage.getItem('alreadyLaunched').then(value => {
+      if(value == null) {
+        AsyncStorage.setItem('alreadyLaunched', 'true');
+        setIsFirstLaunch(true);
+      }
+      else{
+        setIsFirstLaunch(false);
+      }
+    })
+  }, []);
+
+  if(isFirstLaunch === null){
+    return null;
+  }
+  else if(isFirstLaunch === true){
+    return(
+      <AuthScreens></AuthScreens>
+    );
+  }
+  else {
+    return <LoginScreen />
+  }
+
 };
 
 
